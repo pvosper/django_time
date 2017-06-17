a2930c00b2c5cd83e6cdc0387717e4ac6ee741b3
pvosper <pvosper@mac.com>
2017-06-17 08:34:15 -0700

    Morning session work

--- timesite/timeapp/models.py
+++ timesite/timeapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Event(models.Model):
+    title = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+    event_start = models.DateTimeField(auto_now_add=False, auto_now=False)
+    event_end = models.DateTimeField(auto_now_add=False, auto_now=False)
+
+    def __unicode__(self):
+        return self.name
+
+    def __str__(self):
+        return self.name
